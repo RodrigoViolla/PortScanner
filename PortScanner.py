47ccf82f358142329813d3f05af683b8177992d8
Rodrigo Violla <rodrigo_violla-alves@hotmail.com>
2018-01-26 11:59:22 -0200

    first commit

---
+++ PortScanner.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+from socket import *
+import sys, time
+from datetime import datetime
+
+host = ''
+max_port = 5000
+min_port = 0
+
+def scan_host(host, port, r_code = 1):
+	try:
+		s = socket(AF_INET, SOCK_STREAM)
+
+		code = s.connect_ex((host, port))
+
+		if code == 0:
+			r_code = code
+		s.close()
+	except Exception, e:
+		pass
+
+	return r_code
+
+try:
+	host = raw_input("[*] Endereco Alvo: ")
+except KeyboardInterrupt:
+	print("\n[*] Saindo da Aplicacao...")
+	sys.exit(1)
+
+hostip = gethostbyname(host)
+print("\n[*] Host: %s IP: %s" % (host, hostip))
+print("[*] Analizando, Inicio: %s \n" % (time.strftime("%H:%M:%S")))
+start_time = datetime.now()
+
+for port in range(min_port, max_port):
+	try:
+		response = scan_host(hostip, port)
+
+		if not response:
+			print("[*] Porta %d: Aberta" % (port))
+	except Exception, e:
+		pass
+	except KeyboardInterrupt:
+		print("\n[*] Saindo da Aplicacao...")
+		sys.exit(1)
+
+stop_time = datetime.now()
+total_duration = stop_time-start_time
+print("\n[*] Finalizado as %s" % (time.strftime("%H:%M:%S")))
+print("[*] Duracao: %s" % (total_duration))
